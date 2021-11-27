7a701bdacdc4ae0c683e4436dbd331cebcbd87ed
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2021-11-27 13:00:59 +0100

    Fixed

--- RockpaperScissor.py
+++ RockpaperScissor.py
@@ -3,29 +3,29 @@ import os
 import re
 os.system('cls' if os.name=='nt' else 'clear')
 while (1 < 2):
-    print "\n"
-    print "Rock, Paper, Scissors - Shoot!"
-    userChoice = raw_input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
+    print ("\n")
+    print ("Rock, Paper, Scissors - Shoot!")
+    userChoice = input("Choose your weapon [R]ock], [P]aper, or [S]cissors: ")
     if not re.match("[SsRrPp]", userChoice):
-        print "Please choose a letter:"
-        print "[R]ock, [S]cissors or [P]aper."
+        print ("Please choose a letter:")
+        print ("[R]ock, [S]cissors or [P]aper.")
         continue
-    // Echo the user's choice
-    print "You chose: " + userChoice
+    
+    print ("You chose: " + userChoice)
     choices = ['R', 'P', 'S']
-    opponenetChoice = random.choice(choices)
-    print "I chose: " + opponenetChoice
-    if opponenetChoice == str.upper(userChoice):
-        print "Tie! "
-    #if opponenetChoice == str("R") and str.upper(userChoice) == "P"
-    elif opponenetChoice == 'R' and userChoice.upper() == 'S':      
-        print "Scissors beats rock, I win! "
+    Opponent = random.choice(choices)
+    print ("I chose: " + Opponent)
+    if Opponent == str.upper(userChoice):
+        print ("Tie! ")
+    #if Opponent == str("R") and str.upper(userChoice) == "P"
+    elif Opponent == 'R' and userChoice.upper() == 'S':      
+        print ("Scissors beats rock, I win! ")
         continue
-    elif opponenetChoice == 'S' and userChoice.upper() == 'P':      
-        print "Scissors beats paper! I win! "
+    elif Opponent == 'S' and userChoice.upper() == 'P':      
+        print ("Scissors beats paper! I win! ")
         continue
-    elif opponenetChoice == 'P' and userChoice.upper() == 'R':      
-        print "Paper beat rock, I win! "
+    elif Opponent == 'P' and userChoice.upper() == 'R':      
+        print ("Paper beat rock, I win! ")
         continue
     else:       
-        print "You win!"
+        print ("You win!")
